0916525e5990655b1c12e01519273373f4b173c1
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-10-19 21:52:42 +0800

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 
 
 function App() {
 
   return (
-    <div className="text-blue-500 font-bold text-4xl text-center">Tailwind
+    <div className="text-blue-500 font-bold text-4xl text-center">Tailwind CSS
 
     </div>
   )
