0e1bed03306931d17233547063db0823bf71a8f6
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-10-19 21:51:23 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div className="text-blue-500 font-bold text-4xl text-center">Tailwind
+
+    </div>
+  )
+}
+
+export default App
